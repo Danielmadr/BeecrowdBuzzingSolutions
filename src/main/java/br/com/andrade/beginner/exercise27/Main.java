ddd2d12339425c198ee2ed46516d169710b47258
Daniel <danielwick12@hotmail.com>
2024-07-11 18:04:42 -0300

    refactor: moving exercises to beginner package

---
+++ src/main/java/br/com/andrade/beginner/exercise27/Main.java
@@ -0,0 +1,9 @@
+package main.java.br.com.andrade.exercises.exercise27;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
