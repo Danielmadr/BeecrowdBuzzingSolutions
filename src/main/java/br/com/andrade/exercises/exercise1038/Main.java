c43457cde419f9e93201f81bb5677e75c7a0499f
Daniel <danielwick12@hotmail.com>
2024-07-11 17:44:55 -0300

    exercise 1038

---
+++ src/main/java/br/com/andrade/exercises/exercise1038/Main.java
@@ -0,0 +1,24 @@
+package main.java.br.com.andrade.exercises.exercise1038;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static double[] prices = {0.00, 4.00, 4.50, 5.00, 2.00, 1.50};
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] values = scanner.nextLine().split(" ");
+
+        int code = Integer.parseInt(values[0]);
+        int quantity = Integer.parseInt(values[1]);
+
+        calculateTotalPrice(code, quantity);
+    }
+
+    private static void calculateTotalPrice(int code, int quantity) {
+        double totalPrice = prices[code] * quantity;
+        System.out.println("Total: R$ " + String.format("%.2f", totalPrice));
+    }
+}
