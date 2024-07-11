47398de81e0623f21404504f088d7d009dc60aea
Daniel <danielwick12@hotmail.com>
2024-07-11 17:43:52 -0300

    exercise 1013

---
+++ src/main/java/br/com/andrade/exercises/exercise1013/Main.java
@@ -0,0 +1,23 @@
+package main.java.br.com.andrade.exercises.exercise1013;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String[] input = new String[3];
+
+        input = sc.nextLine().split(" ");
+
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        int lagerAB = (A + B + Math.abs(A-B))/2;
+        int lager = (lagerAB + C + Math.abs(lagerAB - C))/2;
+
+        System.out.println(lager + " eh o maior");
+    }
+}
