27887c8b3536bb0e8e46e1ebfd9572bdadc79fab
Daniel <danielwick12@hotmail.com>
2024-07-11 17:44:14 -0300

    exercise 1018

---
+++ src/main/java/br/com/andrade/exercises/exercise1018/Main.java
@@ -0,0 +1,21 @@
+package main.java.br.com.andrade.exercises.exercise1018;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int money = 100;
+        int input = sc.nextInt();
+        System.out.println(input);
+
+        do {
+            System.out.println(input / (money) + " nota(s) de R$ " + money + ",00");
+            input %= money;
+            money = money/2 == 25 ? 20 : money/2;
+        }while (money != 0);
+    }
+}
