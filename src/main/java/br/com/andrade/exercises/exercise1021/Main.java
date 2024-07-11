14bf1178e75f552e114c182ac48953b819bce81c
Daniel <danielwick12@hotmail.com>
2024-07-11 17:44:27 -0300

    exercise 1021

---
+++ src/main/java/br/com/andrade/exercises/exercise1021/Main.java
@@ -0,0 +1,26 @@
+package main.java.br.com.andrade.exercises.exercise1021;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String[] values = sc.nextLine().split(" ");
+
+        int A = Integer.parseInt(values[0]);
+        int B = Integer.parseInt(values[1]);
+        int C = Integer.parseInt(values[2]);
+        int D = Integer.parseInt(values[3]);
+
+        if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+
+    }
+}
