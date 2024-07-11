2a9fecd48ddee64eeb096ea840539c93216d2b46
Daniel <danielwick12@hotmail.com>
2024-07-11 17:43:43 -0300

    exercise 1011

---
+++ src/main/java/br/com/andrade/exercises/exercise1011/Main.java
@@ -0,0 +1,20 @@
+package main.java.br.com.andrade.exercises.exercise1011;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.000");
+
+        double pi = 3.14159;
+        int radius = sc.nextInt();
+        double volume = (double) Math.round((((double) 4 / 3) * pi * Math.pow(radius, 3)) * 1000) / 1000;
+        String output = df.format(volume);
+
+        System.out.println("VOLUME = " + output);
+    }
+}
